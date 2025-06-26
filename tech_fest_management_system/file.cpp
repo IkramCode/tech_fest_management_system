381283136f6dad5823922019fc018ab3d5337504
l1f24bscs0309 <L1F24BSCS0309@ucp.edu.pk>
2025-06-26 18:24:39 +0500

    Add date validation function to check format and range

--- tech_fest_management_system/file.cpp
+++ tech_fest_management_system/file.cpp
@@ -30,4 +30,53 @@ int intValidation() {
     return num;
 }
 
+bool validateDate(char* date) {
+    int day = 0, month = 0, year = 0;
+    int i = 0, c = 0;
+    int len = strLen(date);
+    if (len < 8 || len > 10)
+        return false;
 
+    while (date[i] != '/' && date[i] != '\0') {
+        if (date[i] < '0' || date[i] > '9')
+            return false;
+        day = day * 10 + (date[i] - '0');
+        c++;
+        i++;
+    }
+    if (date[i] != '/' || c == 0 || c > 2)
+        return false;
+    i++;
+    c = 0;
+
+    while (date[i] != '/' && date[i] != '\0') {
+        if (date[i] < '0' || date[i] > '9')
+            return false;
+        month = month * 10 + (date[i] - '0');
+        c++;
+        i++;
+    }
+    if (date[i] != '/' || c == 0 || c > 2)
+        return false;
+    i++;
+    c = 0;
+
+    while (date[i] != '\0') {
+        if (date[i] < '0' || date[i] > '9')
+            return false;
+        year = year * 10 + (date[i] - '0');
+        c++;
+        i++;
+    }
+    if (c != 4)
+        return false;
+
+    if (year < 2025 || year > 2035 || month < 1 || month > 12 || day < 1)
+        return false;
+
+    int maxDays[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    if (day > maxDays[month - 1])
+        return false;
+
+    return true;
+}
